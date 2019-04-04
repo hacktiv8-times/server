93522620d52d8021d5d106b85e0490464bad5ccf
ImamMatot <imammatot@gmail.com>
2019-04-04 20:52:13 +0700

    hacktiv8Times server side

--- controller/index.js
+++ controller/index.js
@@ -8,7 +8,7 @@ let instanceNews = axios.create({
 class newsController {
     static getMostEmailedArticle(req,res){
         instanceNews
-         .get(`/topstories/v2/science.json?api-key=${process.env.API_NEWS}`)
+         .get(`/mostpopular/v2/emailed/${req.params.period}.json?api-key=${process.env.API_NEWS}`)
          .then(({ data })=>{
              res.status(200).json(data)
          })
@@ -20,7 +20,7 @@ class newsController {
     }
     static getMostSharedArticle(req,res){
         instanceNews
-         .get(`/mostpopular/v2/shared/1/facebook.json?api-key=${process.env.API_NEWS}`)
+         .get(`/mostpopular/v2/shared/${req.params.period}/${req.params.share_type}.json?api-key=${process.env.API_NEWS}`)
          .then(({ data })=>{
              res.status(200).json(data)
          })
@@ -32,7 +32,19 @@ class newsController {
     }
     static getMostViewedArticle(req,res){
         instanceNews
-         .get(`/mostpopular/v2/viewed/1.json?api-key=${process.env.API_NEWS}`)
+         .get(`/mostpopular/v2/viewed/${req.params.period}.json?api-key=${process.env.API_NEWS}`)
+         .then(({ data })=>{
+             res.status(200).json(data)
+         })
+         .catch(err =>{
+             res.status(500).json({
+                 message : err.message
+             })
+         })
+    }
+    static getPopularSpecificArticle(req,res){
+        instanceNews
+         .get(`/topstories/v2/${req.params.section}.json?api-key=${process.env.API_NEWS}`)
          .then(({ data })=>{
              res.status(200).json(data)
          })
--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const newsControl = require('../controller/index')
 
-router.get(`/emailed`, newsControl.getMostEmailedArticle)
-router.get('/shared', newsControl.getMostSharedArticle)
-router.get('/viewed', newsControl.getMostViewedArticle)
+router.get(`/emailed/:period`, newsControl.getMostEmailedArticle)
+router.get('/shared/:period/:share_type', newsControl.getMostSharedArticle)
+router.get('/viewed/:period', newsControl.getMostViewedArticle)
+router.get('/section/:section', newsControl.getPopularSpecificArticle)
 
 
 
