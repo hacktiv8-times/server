5fa3927c1c35e3fdd63b04e80d626e506b1f0cc0
willyprayogo26 <willyprayogo26@gmail.com>
2019-04-04 22:22:31 +0700

    fix req weather and news

--- controllers/news.js
+++ controllers/news.js
@@ -4,8 +4,8 @@ class NewsController {
     static getNews (req, res) {
         axios
             .get(`https://api.nytimes.com/svc/topstories/v2/science.json?api-key=${process.env.NEWS_API_KEY}`)
-            .then(news => {
-                res.status(200).json(news)
+            .then(({ data }) => {
+                res.status(200).json(data)
             })
             .catch(err => {
                 res.status(500).json(err)
--- controllers/weather.js
+++ controllers/weather.js
@@ -4,8 +4,8 @@ class WeatherController {
     static getWeather(req, res) {
         axios
             .get(`https://api.openweathermap.org/data/2.5/weather?id=${process.env.WEATHER_CITY_ID}&appid=${process.env.WEATHER_OWN_APP_ID}`)
-            .then(weather => {
-                res.status(200).json(weather)
+            .then(({ data }) => {
+                res.status(200).json(data)
             })
             .catch(err => {
                 res.status(500).json(err)
