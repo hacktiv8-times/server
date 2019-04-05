9e5b8f9f0c11fd3fe352131d860e5625db289d5d
willyprayogo26 <willyprayogo26@gmail.com>
2019-04-05 11:54:03 +0700

    add youtube features

--- controllers/youtube.js
+++ controllers/youtube.js
@@ -4,8 +4,8 @@ class YoutubeController {
     static getVideos (req, res) {
         axios
             .get(`https://www.googleapis.com/youtube/v3/search?part=snippet&key=${process.env.YOUTUBE_API_KEY}&maxResults=3&q=${req.params.title}`)
-            .then(data => {
-                res.status(200).json(data)
+            .then(({ data }) => {
+                res.status(200).json(data.items)
             })
             .catch(err => {
                 res.status(500).json(err)
